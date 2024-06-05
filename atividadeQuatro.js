197c817b0be16b33562347bc6007547ef31916b3
brendaMaganeli <brenda_casimiro@estudante.sesisenai.org.br>
2024-06-05 15:38:11 -0300

    ConversaoCelsiusParaFahrenheit

---
+++ atividadeQuatro.js
@@ -0,0 +1,21 @@
+//4) Programar a conversão de uma temperatura digitada pelo usuário em graus Celsius para Fahrenheit. Mostrar o resultado na tela.
+
+// variáveis
+
+let temperaturaDigitada
+let conversaoCelsiusParaFahrenheit
+
+// entrada de dados
+
+temperaturaDigitada = prompt('Digite a temperatura que deseja converter: ')
+
+// cálculos
+
+conversaoCelsiusParaFahrenheit = temperaturaDigitada *  9/5 + 32 
+
+
+
+// saída de dados
+
+alert('temperatura em farhenheit: ' + conversaoCelsiusParaFahrenheit)
+
